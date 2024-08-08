b28c67599de3de9ae590b45d5738820b2434abd4
Joseph Moyalan <joseph.moyalan@coinbase.com>
2024-08-08 14:49:25 -0700

    Added updates

--- auction-app/src/AuctionManager.sol
+++ auction-app/src/AuctionManager.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract AuctionManager{
+contract AuctionManager is IERC721Receiver{
 
     address private brokerAddress;
     uint private auctionId;
@@ -53,6 +55,20 @@ contract AuctionManager{
         }
         return localArray;
     }
+
+    function getActiveAuctions(uint _time) public view returns (uint[] memory){
+        uint[] memory localArray = new uint[](auctionIds.length);
+        uint x = 0;
+        for (uint i = 0; i < auctionIds.length; i++) {
+            Auction memory currAuction = availableAuctions[auctionIds[i]];
+            if (_time <= currAuction.endTime &&  _time >= currAuction.startTime){
+                localArray[x] = auctionIds[i];
+                x+=1;
+            }
+        }
+        return localArray;
+    } 
+
     function claim(uint _auctionId, address _userAddress)public {
         Auction storage currAuction = availableAuctions[_auctionId];
         require(_userAddress == currAuction.highestBidder, "Only highest bidder can claim");
@@ -85,5 +101,9 @@ contract AuctionManager{
             }
         }
         require(found, "Value not found in the array");
+    }
+
+    function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
     }
 }
